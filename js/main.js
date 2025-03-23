b80fc1a3ceeb098dfb9708d3cb0dc30212e7cdf6
quarterback <contact@ronbronson.com>
2025-03-22 23:28:20 -0700

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+fetch('articles.json')
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById('article-list');
+    data.forEach(article => {
+      const block = document.createElement('div');
+      block.className = 'article-preview';
+      block.innerHTML = `
+        <h2><a href="${article.url}">${article.title}</a></h2>
+        <p class="byline">${article.date}</p>
+        <p>${article.excerpt}</p>
+      `;
+      container.appendChild(block);
+    });
+  });
